02c6eb347c7fb275766af813c49b059994e4e154
정대현 <eogus_2513@dsm.hs.kr>
2022-05-30 08:40:35 +0900

    :bookmark_tabs: :: GlobalExceptionHandler

---
+++ src/main/java/com/xquare/assignment/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.xquare.assignment.global.error;
+
+import com.xquare.assignment.global.error.ErrorResponse.BindErrorResponse;
+import com.xquare.assignment.global.error.ErrorResponse.CustomErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<CustomErrorResponse> customExceptionHandling(CustomException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(
+                CustomErrorResponse.builder()
+                        .status(errorCode.getStatus())
+                        .message(errorCode.getMessage())
+                        .build(),
+                HttpStatus.valueOf(errorCode.getStatus())
+        );
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<BindErrorResponse> bindExceptionHandling(BindException e) {
+        List<String> errorsList = new ArrayList<>();
+
+        for (FieldError error : e.getFieldErrors()) {
+            errorsList.add(error.getDefaultMessage());
+        }
+        BindErrorResponse errorResponse = BindErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .messages(errorsList)
+                .build();
+
+        return new ResponseEntity<BindErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
