7e2397cfdd58d40b6434ee59efe5536d76b85c9d
정대현 <eogus_2513@dsm.hs.kr>
2022-05-30 08:41:13 +0900

    :bookmark_tabs: :: ExceptionFilter

---
+++ src/main/java/com/xquare/assignment/global/error/ExceptionFilter.java
@@ -0,0 +1,42 @@
+package com.xquare.assignment.global.error;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xquare.assignment.global.error.ErrorResponse.CustomErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (CustomException e) {
+            sendErrorMessage(response, e.getErrorCode());
+        } catch (Exception e) {
+            logger.error(e);
+            sendErrorMessage(response, ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void sendErrorMessage(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        CustomErrorResponse customErrorResponse = CustomErrorResponse.builder()
+                .status(errorCode.getStatus())
+                .message(errorCode.getMessage())
+                .build();
+        String errorResponseJson = objectMapper.writeValueAsString(customErrorResponse);
+
+        response.setStatus(errorCode.getStatus());
+        response.setContentType("application/json");
+        response.getWriter().write(errorResponseJson);
+    }
+}
