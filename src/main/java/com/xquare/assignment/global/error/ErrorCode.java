b2b77e1506c90d8fe7ab07bcdecc9706ee9fb8bb
정대현 <eogus_2513@dsm.hs.kr>
2022-05-30 08:38:58 +0900

    :bookmark_tabs: :: ErrorCode

---
+++ src/main/java/com/xquare/assignment/global/error/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.xquare.assignment.global.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int status;
+    private final String message;
+
+}
