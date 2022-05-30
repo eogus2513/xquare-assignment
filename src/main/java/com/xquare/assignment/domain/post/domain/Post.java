331a12abe91e5ed05157383d64d7edbef0d6c342
정대현 <eogus_2513@dsm.hs.kr>
2022-05-31 08:32:31 +0900

    :bookmark_tabs: :: Post

---
+++ src/main/java/com/xquare/assignment/domain/post/domain/Post.java
@@ -0,0 +1,50 @@
+package com.xquare.assignment.domain.post.domain;
+
+import com.xquare.assignment.domain.user.domain.User;
+import com.xquare.assignment.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Post(String title, String content, LocalDateTime updatedAt, User user) {
+        this.title = title;
+        this.content = content;
+        this.updatedAt = updatedAt;
+        this.user = user;
+    }
+}
