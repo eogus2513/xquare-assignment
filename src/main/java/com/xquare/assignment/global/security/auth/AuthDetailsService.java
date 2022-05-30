10f084f56d751355b34d97346dcf9c2efbcb7ce0
정대현 <eogus_2513@dsm.hs.kr>
2022-05-31 08:27:51 +0900

    :bookmark_tabs: :: AuthDetailsService

---
+++ src/main/java/com/xquare/assignment/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,27 @@
+package com.xquare.assignment.global.security.auth;
+
+import com.xquare.assignment.domain.user.domain.repository.UserRepository;
+import com.xquare.assignment.global.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+}
