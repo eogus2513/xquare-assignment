d0acdf9d69b35090cb5241890776d1118eea14fc
정대현 <eogus_2513@dsm.hs.kr>
2022-05-30 16:07:54 +0900

    :recycle: :: @ConfigurationPropertiesScan을 config 파일로 수정

--- src/main/java/com/xquare/assignment/AssignmentApplication.java
+++ src/main/java/com/xquare/assignment/AssignmentApplication.java
@@ -2,9 +2,7 @@ package com.xquare.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class AssignmentApplication {
 
