66e746ddc986c8815c168b796e39a1909e04f861
정대현 <eogus_2513@dsm.hs.kr>
2022-06-01 20:26:01 +0900

    :coffin: :: RefreshToken 파일

--- src/main/java/com/xquare/assignment/domain/user/domain/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xquare.assignment.domain.user.domain.repository;
-
-import com.xquare.assignment.domain.user.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
