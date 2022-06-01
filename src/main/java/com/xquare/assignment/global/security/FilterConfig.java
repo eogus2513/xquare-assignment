1764def7f467ad460b17815dec92ada5a35c38ae
정대현 <eogus_2513@dsm.hs.kr>
2022-06-01 23:46:48 +0900

    :bookmark_tabs: :: FilterConfig

---
+++ src/main/java/com/xquare/assignment/global/security/FilterConfig.java
@@ -0,0 +1,21 @@
+package com.xquare.assignment.global.security;
+
+import com.xquare.assignment.global.security.jwt.JwtTokenFilter;
+import com.xquare.assignment.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
