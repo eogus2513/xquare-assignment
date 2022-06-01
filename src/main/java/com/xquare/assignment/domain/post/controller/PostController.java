e2146e3ef39711569a0e2737f675a084e8889abb
정대현 <eogus_2513@dsm.hs.kr>
2022-06-01 23:45:57 +0900

    ⚡️ :: 게시글 생성 Api 추가

--- src/main/java/com/xquare/assignment/domain/post/controller/PostController.java
+++ src/main/java/com/xquare/assignment/domain/post/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.xquare.assignment.domain.post.controller;
 
+import com.xquare.assignment.domain.post.controller.dto.request.CreatePostRequest;
 import com.xquare.assignment.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/post")
@@ -11,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createPost(@RequestBody @Valid CreatePostRequest request) {
+        postService.createPost(request);
+    }
 }
--- src/main/java/com/xquare/assignment/domain/post/service/PostService.java
+++ src/main/java/com/xquare/assignment/domain/post/service/PostService.java
@@ -1,6 +1,10 @@
 package com.xquare.assignment.domain.post.service;
 
+import com.xquare.assignment.domain.client.global.domain.Client;
+import com.xquare.assignment.domain.post.controller.dto.request.CreatePostRequest;
+import com.xquare.assignment.domain.post.domain.Post;
 import com.xquare.assignment.domain.post.domain.repository.PostRepository;
+import com.xquare.assignment.global.facade.CurrentFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +13,16 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final CurrentFacade currentFacade;
+
+    public void createPost(CreatePostRequest request) {
+        Client client = currentFacade.getCurrentClient();
+
+        Post post = Post.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .client(client)
+                .build();
+        postRepository.save(post);
+    }
 }
