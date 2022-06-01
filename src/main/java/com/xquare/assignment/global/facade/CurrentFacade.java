2ad13f0fafb5a45fc4223112f2aadbb6c6bd95ca
정대현 <eogus_2513@dsm.hs.kr>
2022-06-01 23:49:27 +0900

    :bookmark_tabs: :: CurrentFacade

---
+++ src/main/java/com/xquare/assignment/global/facade/CurrentFacade.java
@@ -0,0 +1,21 @@
+package com.xquare.assignment.global.facade;
+
+import com.xquare.assignment.domain.client.global.domain.Client;
+import com.xquare.assignment.domain.client.global.domain.repository.ClientRepository;
+import com.xquare.assignment.global.exception.ClientNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CurrentFacade {
+
+    private final ClientRepository clientRepository;
+
+    public Client getCurrentClient() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return clientRepository.findByAccountId(accountId)
+                .orElseThrow(() -> ClientNotFoundException.EXCEPTION);
+    }
+}
