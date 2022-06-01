c86c7518cd858d7af9faf45bd3648c0deb85b194
정대현 <eogus_2513@dsm.hs.kr>
2022-06-01 21:24:06 +0900

    :recycle: :: @ResponseStatus(HttpStatus.CREATED) 추가

--- src/main/java/com/xquare/assignment/domain/client/user/controller/UserController.java
+++ src/main/java/com/xquare/assignment/domain/client/user/controller/UserController.java
@@ -6,9 +6,11 @@ import com.xquare.assignment.domain.client.global.dto.request.SignUpRequest;
 import com.xquare.assignment.domain.client.global.dto.response.TokenResponse;
 import com.xquare.assignment.domain.client.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -26,6 +28,7 @@ public class UserController implements BaseClient {
         return userService.signIn(request);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public void signUp(@RequestBody @Valid SignUpRequest request) {
         userService.signUp(request);
