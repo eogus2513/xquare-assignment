bdaa4b16bfc66fadab97feb3f518e726ab5a4bae
정대현 <eogus_2513@dsm.hs.kr>
2022-06-03 12:01:03 +0900

    :recycle: :: client -> auth로 변경

--- src/main/java/com/xquare/assignment/domain/auth/user/exception/AuthExistsException.java
+++ src/main/java/com/xquare/assignment/domain/auth/user/exception/AuthExistsException.java
@@ -3,12 +3,12 @@ package com.xquare.assignment.domain.auth.user.exception;
 import com.xquare.assignment.global.error.CustomException;
 import com.xquare.assignment.global.error.ErrorCode;
 
-public class ClientExistsException extends CustomException {
+public class AuthExistsException extends CustomException {
 
     public static final CustomException EXCEPTION =
-            new ClientExistsException();
+            new AuthExistsException();
 
-    private ClientExistsException() {
-        super(ErrorCode.CLIENT_EXISTS);
+    private AuthExistsException() {
+        super(ErrorCode.AUTH_EXISTS);
     }
 }
